0f341f39486a3ea3943b1dcb130bad79df4aef1b
estelaV9 <esteladeveg@gmail.com>
2024-09-29 15:39:53 +0200

    crear y diseñar la parte de listar cuentas. Crear su respectivo controller con sus atributos

---
+++ introduccion/Cuentas_Bancarias/src/main/java/com/example/cuentas_bancarias/Controller/ListAccountCtrller.java
@@ -0,0 +1,26 @@
+package com.example.cuentas_bancarias.Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+
+public class ListAccountCtrller {
+
+    @FXML
+    private Pane accountPane;
+
+    @FXML
+    private ImageView volverBtt;
+
+    @FXML
+    void onExitAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onVolverAction(MouseEvent event) {
+
+    }
+
+}
