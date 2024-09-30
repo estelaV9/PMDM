d6d1a1500ad972d78c4736615c815952a5f414af
estelaV9 <esteladeveg@gmail.com>
2024-09-30 20:00:16 +0200

    Modificar el metodo informacionCuenta (se quita la variable booleana existeCuenta y el mensaje de retorno, se simplifica el codigo sin las variables innecesarias y se hace de forma directa con el return)

--- introduccion/Cuentas_Bancarias/src/main/java/com/example/cuentas_bancarias/Model/Banco.java
+++ introduccion/Cuentas_Bancarias/src/main/java/com/example/cuentas_bancarias/Model/Banco.java
@@ -36,20 +36,13 @@ public class Banco {
     } // LISTADO DE CUENTAS QUE DEVUELVE UN ARRAY DE CADENAS DE LA INFORMACION DE LAS CUENTAS
 
     public String informacionCuenta(String IBAN) {
-        boolean existeCuenta = false; // VARIABLE PARA SABER SI EXISTE UNA CUENTA O NO
-        String mensajeInfoCuenta = ""; // VARIABLE PARA GUARDAR EL MENSAJE DE LA INFORMACINO DE CUENTA
         for (CuentaBancaria cuentaBancaria : this.cuentasLista) {
             if (cuentaBancaria.getNumCuenta().equals(IBAN)) {
-                existeCuenta = true;
-                // SI EXISTE UNA CUENTA CON ESE IBAN DEVOLVERA LA INFO DE ESA CUENTA Y SALDRA DEL BUCLE
-                mensajeInfoCuenta = cuentaBancaria.devolverInfoString();
-                break;
-            }
-        }
-        if (!existeCuenta) {
-            mensajeInfoCuenta = "null";
-        } // SI NO EXISTE LA CUENTA, EL MENSAJE DEVOLVERA NULL
-        return mensajeInfoCuenta; // RETORNARA EL MENSAJE DE LA INFORMACION
+                 return cuentaBancaria.devolverInfoString(); // RETORNA LA INFO Y SALE DEL METODO
+            } // SI EXISTE UNA CUENTA CON ESE IBAN, DEVOLVERA LA INFO DE ESA CUENTA Y SALDRA
+        } // RECORREMOS LA LISTA DE CUENTAS
+        // SI NO ENCUENTRA NINGUNA CUENTA CON ESE IBAN RETORNARA DIRECTAMENTE NULL
+        return "null";
     } // INFORMACION CUENTAS QUE DEVUELVE INFORMACION DE LA CUENTA PASADA POR PARAMETRO
 
 
