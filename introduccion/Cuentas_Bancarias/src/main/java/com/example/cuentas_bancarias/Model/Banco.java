bca4166b857541f8dbbad503af24552dfa7e6702
estelaV9 <esteladeveg@gmail.com>
2024-09-30 19:52:12 +0200

    Modificar el metodo abrirCuenta (se quita la variable booleana operacionExitosa)

--- introduccion/Cuentas_Bancarias/src/main/java/com/example/cuentas_bancarias/Model/Banco.java
+++ introduccion/Cuentas_Bancarias/src/main/java/com/example/cuentas_bancarias/Model/Banco.java
@@ -11,24 +11,16 @@ public class Banco {
     } // CONSTRUCTOR SIN PARAMETROS PARA CREAR UN OBJETO DE TIPO BANCO DONDE SE CREE EL ARRAYLIST
 
     public boolean abrirCuenta(CuentaBancaria cuentaBancaria) {
-        boolean operacionExitosa = true; // ATRIBUTO QUE INDICA SI LA OPERACION HA SIDO EXITOSA O NO
-
-        if (this.cuentasLista.isEmpty()) {
-            this.cuentasLista.add(cuentaBancaria); // SE AÑADE LA PRIMERA CUENTA
-            return true; // LA OPERACION FUE EXITOSA
-        } // SI LA LISTA ESTA VACIA, SE AÑADE DIRECTAMENTE
-
+        // SE USA DIRECTAMENTE EL RETURN FALSE Y TRUE PARA EVITAR UNA VARIABLE TIPO BOOLEAN ADICIONAL
         for (CuentaBancaria CB : this.cuentasLista) {
             if (CB.getNumCuenta().equals(cuentaBancaria.getNumCuenta())) {
-                operacionExitosa = false;
-                break;
-            } // SI EXISTE LA CUENTA, LA OPERACION NO HA SIDO EXITOSA
-        }
-        if (operacionExitosa) {
-            this.cuentasLista.add(cuentaBancaria);
-        } // SI NO EXISTE LA CUENTA, SE AÑADE
+                return false; // DEVUELVE FALSE Y SALE DEL METODO
+            } // SE COMPRUEBA SI LA CUENTA EXISTE
+        } // RECORREMOS LAS LISTAS EXISTENTES
 
-        return operacionExitosa;
+        // SI NO SE ENCONTRO NINGUNA CUENTA CON EL MISMO NUMERO, SE AÑADE A LA LISTA
+        this.cuentasLista.add(cuentaBancaria); // SI NO EXISTE LA CUENTA, SE AÑADE
+        return true; // LA OPERACION FUE EXITOSA
     } // DEVUELVE TRUE O FALSE SI ALMACENAR LA CUENTA HA SIDO EXITOSA O NO
 
     public String[] listadoCuentas() {
@@ -43,44 +35,44 @@ public class Banco {
         return cuentasInfo;  // SE DEVUELVE EL ARRAY DE STRING
     } // LISTADO DE CUENTAS QUE DEVUELVE UN ARRAY DE CADENAS DE LA INFORMACION DE LAS CUENTAS
 
-    public String informacionCuenta(String IBAN){
+    public String informacionCuenta(String IBAN) {
         boolean existeCuenta = false; // VARIABLE PARA SABER SI EXISTE UNA CUENTA O NO
         String mensajeInfoCuenta = ""; // VARIABLE PARA GUARDAR EL MENSAJE DE LA INFORMACINO DE CUENTA
-        for(CuentaBancaria cuentaBancaria: this.cuentasLista){
-            if(cuentaBancaria.getNumCuenta().equals(IBAN)){
+        for (CuentaBancaria cuentaBancaria : this.cuentasLista) {
+            if (cuentaBancaria.getNumCuenta().equals(IBAN)) {
                 existeCuenta = true;
                 // SI EXISTE UNA CUENTA CON ESE IBAN DEVOLVERA LA INFO DE ESA CUENTA Y SALDRA DEL BUCLE
                 mensajeInfoCuenta = cuentaBancaria.devolverInfoString();
                 break;
             }
         }
-        if(!existeCuenta){
+        if (!existeCuenta) {
             mensajeInfoCuenta = "null";
         } // SI NO EXISTE LA CUENTA, EL MENSAJE DEVOLVERA NULL
         return mensajeInfoCuenta; // RETORNARA EL MENSAJE DE LA INFORMACION
     } // INFORMACION CUENTAS QUE DEVUELVE INFORMACION DE LA CUENTA PASADA POR PARAMETRO
 
 
-    public boolean ingresoCuenta(String IBAN, double cantIngreso){
+    public boolean ingresoCuenta(String IBAN, double cantIngreso) {
         boolean existeCuenta = false; // VARIABLE PARA SABER SI EXISTE UNA CUENTA O NO
         double cantTotal; // VARIABLE PARA GUARDAR LA CANTIDAD TOTAL DE LA CUENTA (actual + ingresoNuevo)
-        for(CuentaBancaria cuentaBancaria: this.cuentasLista){
-            if(cuentaBancaria.getNumCuenta().equals(IBAN)){
+        for (CuentaBancaria cuentaBancaria : this.cuentasLista) {
+            if (cuentaBancaria.getNumCuenta().equals(IBAN)) {
                 existeCuenta = true;
                 cantTotal = cuentaBancaria.getSaldoActualCuenta() + cantIngreso;
                 cuentaBancaria.setSaldoActualCuenta(cantTotal);
                 break;
             } // SI EXISTE LA CUENTA ENTONCES SE SETTEA EL VALOR DEL SALDO Y SE SALE DEL BUCLE
         } // FOR PARA RECORRE LA CUENTA
-        return  existeCuenta;
+        return existeCuenta;
     } // METODO PARA EL INGRESO DE UNA CANTIDAD EN LA CUENTA
 
-    public boolean retiradaCuenta (String IBAN, double cantRetirar){
+    public boolean retiradaCuenta(String IBAN, double cantRetirar) {
         boolean existeCuenta = false; // VARIABLE PARA SABER SI EXISTE UNA CUENTA O NO
         double cantTotal; // VARIABLE PARA GUARDAR LA CANTIDAD TOTAL DE LA CUENTA (actual - cantRetirar)
-        for(CuentaBancaria cuentaBancaria: this.cuentasLista){
-            if(cuentaBancaria.getNumCuenta().equals(IBAN)){
-                if ((cuentaBancaria.getSaldoActualCuenta() - cantRetirar) <0) {
+        for (CuentaBancaria cuentaBancaria : this.cuentasLista) {
+            if (cuentaBancaria.getNumCuenta().equals(IBAN)) {
+                if ((cuentaBancaria.getSaldoActualCuenta() - cantRetirar) < 0) {
                     // SI LA CANTIDAD A RETIRAR ES MAYOR A LA CANTIDAD QUE TENGA EN LA CUENTA, ENTONCES SE SALE DEL BUCLE (en modo false)
                     break;
                 } else if (!(cantRetirar < 0)) {
@@ -91,17 +83,17 @@ public class Banco {
                 break;
             } // SI EXISTE LA CUENTA ENTONCES SE SETTEA EL VALOR DEL SALDO Y SE SALE DEL BUCLE
         } // FOR PARA RECORRE LA CUENTA
-        return  existeCuenta;
+        return existeCuenta;
     } // METODO PARA RETIRAR UNA CANTIDAD DE UNA CUENTA
 
-    public double obtenerSaldo (String IBAN){
+    public double obtenerSaldo(String IBAN) {
         double existeCuenta = -1; // VARIABLE PARA SABER SI EXISTE UNA CUENTA O NO. SI NO EXISTE DEVUELVE -1
-        for(CuentaBancaria cuentaBancaria: this.cuentasLista){
-            if(cuentaBancaria.getNumCuenta().equals(IBAN)){
+        for (CuentaBancaria cuentaBancaria : this.cuentasLista) {
+            if (cuentaBancaria.getNumCuenta().equals(IBAN)) {
                 existeCuenta = 0;
                 return cuentaBancaria.getSaldoActualCuenta();
             } // SI EXISTE LA CUENTA ENTONCES SE SETTEA EL VALOR DEL SALDO Y SE SALE DEL BUCLE
         } // FOR PARA RECORRE LA CUENTA
-        return  existeCuenta;
+        return existeCuenta;
     } // METODO PARA OBTENER EL SALDO DE UNA CUENTA
 }
