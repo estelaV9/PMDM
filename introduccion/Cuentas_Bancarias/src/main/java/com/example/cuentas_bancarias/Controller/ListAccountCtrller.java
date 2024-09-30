5d644ca9a5abfb31c75da434618242135017ee87
estelaV9 <esteladeveg@gmail.com>
2024-09-30 20:49:59 +0200

    Añadir comentario mas detallado de la pagina de ListAccountCtrller

--- introduccion/Cuentas_Bancarias/src/main/java/com/example/cuentas_bancarias/Controller/ListAccountCtrller.java
+++ introduccion/Cuentas_Bancarias/src/main/java/com/example/cuentas_bancarias/Controller/ListAccountCtrller.java
@@ -17,11 +17,15 @@ public class ListAccountCtrller implements Initializable {
 
     @FXML
     void onExitAction(MouseEvent event) {
+        // SE LLAMA AL METODO ESTATICO PARA SALIR DE LA APLICACION
         StaticCode.exitApp();
     } // SALIR DE LA APLICACIÓN
 
     @FXML
     void onVolverAction(MouseEvent event) {
+        // SE LLAMA AL METODO ESTATICO CAMBIAR VISTA POR IMAGEN PARA VOLVER A LA PAGINA PRINCIPAL
+        // SE INSERTA LOS PARAMETROS: NOMBRE DEL FXML AL QUE SE QUIERE IR, UN IMAGENVIEW Y
+        // EL TITULO QUE VA A TENER ESE STAGE
         StaticCode.cambiarVistaImg("Start.fxml", volverBtt, "Start Application");
     } // IR A LA PAGINA PRINCIPAL DE LA APLICACION
 
@@ -30,12 +34,14 @@ public class ListAccountCtrller implements Initializable {
         // SE LE AÑADE MANUALMENTE EL ESTILO DEL FONDO Y DEL TEXTO
         /*** en el textArea no sirve cambiar el color de fondo con background (cambia el color del borde),
          * asique se pone control-inner-background para cambiar el color de fondo ***/
-        contentPaneTxt.setStyle("-fx-control-inner-background: #56269c; -fx-text-fill: #FFFFFF; -fx-font-size: 17; -fx-font-family: 'Berlin Sans FB';");
-        String[] accountsList = StaticCode.banco.listadoCuentas();
+        contentPaneTxt.setStyle("-fx-control-inner-background: #56269c; -fx-text-fill: #FFFFFF; -fx-font-size: 17; " +
+                "-fx-font-family: 'Berlin Sans FB';");
+        String[] accountsList = StaticCode.banco.listadoCuentas(); // SE CREA LA LISTA CON EL LISTADO DE CEUNTAS
         for (String accountInfo : accountsList) {
             // SE USA APPEND PARA AGREGAR EL NUEVO TEXTO AL CONTENIDO ACTUAL.
             // YA QUE CON setText() REEMPLAZA EL CONTENIDO
             contentPaneTxt.appendText(accountInfo + "\n\n");
-        }
-    }
+        } // FOR PARA SE RECORRE LAS CUENTAS Y SE VA AÑADIENDO LA INFORMACION
+    } // AL INICIAR ESTA VISTA, SE PONE EL FONDO DEL TEXTAREA EN MORADO, EL COLOR EN BLANCO,
+    // CON UN TAMAÑO DE 17 CON FUENTE Berlin Sans FB
 }
