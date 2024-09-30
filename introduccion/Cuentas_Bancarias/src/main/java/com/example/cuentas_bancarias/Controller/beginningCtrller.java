5784fb0594959a4298d8f51fef9dc7a3236e30ad
estelaV9 <esteladeveg@gmail.com>
2024-09-30 20:20:54 +0200

    Añadir comentario mas detallado de la pagina de beginningCtrller

--- introduccion/Cuentas_Bancarias/src/main/java/com/example/cuentas_bancarias/Controller/beginningCtrller.java
+++ introduccion/Cuentas_Bancarias/src/main/java/com/example/cuentas_bancarias/Controller/beginningCtrller.java
@@ -10,6 +10,8 @@ public class beginningCtrller {
     private Button startAppBtt;
     @FXML
     void onStartAppAction(ActionEvent event) {
+        // SE LLAMA AL METODO ESTATICO CAMBIAR VISTA POR BOTON PARA IR A LA PAGINA PRINCIPAL
+        // SE INSERTA LOS PARAMETROS: NOMBRE DEL FXML AL QUE SE QUIERE IR, UN BOTON Y EL TITULO QUE VA A TENER ESE STAGE
         StaticCode.cambiarVistaBtt("Start.fxml", startAppBtt, "Start Application");
     } // IR A LA PAGINA PRINCIPAL DE LA APLICACION
 }
