e85eeae203e92566b7e1a4280cc502c7fe01e743
estelaV9 <esteladeveg@gmail.com>
2024-10-14 18:14:11 +0200

    EJERCICIO TERMINADO: añadir las funciones dentro del when

--- Tema2_Kotlin/Ejercicios/EjerciciosBasicos/Ejercicio2_AdivinarNumeroOculto/src/main/kotlin/Main.kt
+++ Tema2_Kotlin/Ejercicios/EjerciciosBasicos/Ejercicio2_AdivinarNumeroOculto/src/main/kotlin/Main.kt
@@ -4,19 +4,21 @@ import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
     val scanner = Scanner(System.`in`) // DECLARAR EN UNA VARIABLE VAL EL SCANNER
-    var opcion:Int = -1
-    var numIntentos:Int
-    var numMaxGenerado = 0
-    var numeroAleatorio:Int // GUARDAR EL NUMERO ALEATORIO
+    var opcion: Int = -1
+    var numIntentos = 5 // valores por defecto
+    var numMaxGenerado = 10 // valores por defecto
+    var numeroAleatorio: Int // GUARDAR EL NUMERO ALEATORIO
+    var numIntroducido: Int // GUARDAR EL NUMERO QUE VAYA A INTRODUCIR EL USUARIO
     print("*********** Adivinar el número oculto ***********")
     while (opcion != 3) {
         println("MENU" +
             "\n1 - Configurar." +
             "\n2 - Jugar." +
-            "\n3 - Salir.")
+            "\n3 - Salir."
+        )
         println("Introducir opcion: ")
         opcion = scanner.nextInt()
-        when (opcion){
+        when (opcion) {
             1 -> {
                 println("********* CONFIGURAR *********")
                 println("¿Cuántos intentos deseas permitir?")
@@ -24,21 +26,37 @@ fun main(args: Array<String>) {
                 println("¿Cual es el número máximo a adivinar?")
                 numMaxGenerado = scanner.nextInt()
             }
+
             2 -> {
                 println("******** JUGAR *******")
-                if (numMaxGenerado == 0) {
-                    println("No has introducido ningún numero máximo a adivinar")
+                // GENERAR UN NUMERO ALEATORIOENTRE 0 Y 9 (EXCLUYENDO EL 10)
+                numeroAleatorio = Random.nextInt(numMaxGenerado + 1)
+                println("Introduzca numeros para ver si adivinas")
+                numIntroducido = scanner.nextInt()
+                if (numIntroducido == numeroAleatorio) {
+                    // Si el usuario adivina el numero, se muestra un mensaje
+                    println("Has ganado! Has necesitado $numIntentos")
                 } else {
-                    // GENERAR UN NUMERO ALEATORIOENTRE 0 Y 9 (EXCLUYENDO EL 10)
-                    numeroAleatorio = Random.nextInt(numMaxGenerado + 1)
-
-                    println("Número aleatorio: $numeroAleatorio")
+                    numIntentos--
+                    if (numIntroducido < numeroAleatorio) {
+                        println("PISTA: el número oculto es mayor")
+                    } else {
+                        println("PISTA: el número oculto es menor")
+                    }
+                }
+                if (numIntentos == 0) {
+                    println("¡Perdiste! Se acabaron los intentos. El número era: $numeroAleatorio.")
+                } else {
+                    println("Te quedan $numIntentos intentos.")
                 }
             }
+
             3 -> {
                 println("¡Hasta pronto!")
-                exitProcess(0)
+                exitProcess(0) // TERMINAR PROGRAMA
             }
+
+            else -> println("Opción no válida. Elige una opción del menú.")
         }
     }
 }
