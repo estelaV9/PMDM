ef18a4bd2e041d19b686df59d61c99f29353f5ed
estelaV9 <esteladeveg@gmail.com>
2024-10-14 18:34:40 +0200

    EJERCICIO ACABADO: hacer el ejercicio 4

--- Tema2_Kotlin/Ejercicios/EjerciciosBasicos/Ejercicio3_NumDigitosParesImpares/src/main/kotlin/Main.kt
+++ Tema2_Kotlin/Ejercicios/EjerciciosBasicos/Ejercicio3_NumDigitosParesImpares/src/main/kotlin/Main.kt
@@ -1,3 +1,26 @@
-fun main(args: Array<String>) {
+import java.util.Scanner
 
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+    var contadorPares = 0
+    var contadorImpares = 0
+    var contadorDigitos = 0
+
+    println("Introduzca un numero: ")
+    val numIntroducido:Int = scanner.nextInt()
+    var numAuxiliar = numIntroducido // VARIABLE AUXILIAR PARA IR REDUCIENDO
+
+    while (numAuxiliar > 0) {
+        val digito = numAuxiliar % 10 // EXTRAER EL ULTIMO DIGITO
+        contadorDigitos++
+        if (digito % 2 == 0) {
+            contadorPares++
+        } else {
+            contadorImpares++
+        }
+        numAuxiliar = numAuxiliar / 10 // ELIMINAR EL ULTIMO DIGITO
+    }
+    println("El numero $numIntroducido tiene $contadorDigitos digitos " +
+            "los cuales $contadorPares son digitos pares y $contadorImpares " +
+            "son digitos impares")
 }
