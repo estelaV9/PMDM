1c309ff8f4074b86a02f70d5aec84a1a59b88e52
estelaV9 <esteladeveg@gmail.com>
2024-10-14 19:56:39 +0200

    EJERCICIO TERMINADO: hacer ejercicio 8 numeros primos

--- Tema2_Kotlin/Ejercicios/EjerciciosBasicos/Ejercicio8_NumerosPrimos/src/main/kotlin/Main.kt
+++ Tema2_Kotlin/Ejercicios/EjerciciosBasicos/Ejercicio8_NumerosPrimos/src/main/kotlin/Main.kt
@@ -1,3 +1,29 @@
+import java.time.temporal.ValueRange
+import java.util.Scanner
+
 fun main(args: Array<String>) {
     // Defina una función Kotlin que devuelva si un número es primo o no.
+    val scanner = Scanner (System.`in`)
+    println("Introduzca un numero: ")
+    var numero = scanner.nextInt()
+    if(esNumeroPrimo(numero)){
+        println("$numero es un numero primo")
+    } else {
+        println("$numero NO es un numero primo")
+    } // COMPROBAR SI ES PRIMO O NO
+}
+
+fun esNumeroPrimo (numero:Int): Boolean{
+    if (numero <= 1) {
+        return false
+    } // SI INTRODUCE UN NUMERO NEGATIVO, NO SERA PRIMO
+
+    val raizCuadrada = Math.sqrt(numero.toDouble()).toInt() // RAIZ CUADRADA
+    println(raizCuadrada)
+    for (i:Int in 2..raizCuadrada){
+        if (numero % i == 0) {
+            return false
+        } // SI HAY DIVISOR, NO ES PRIMO, SALE DEL BUCLE
+    }
+    return true // SI NO HAY DIVISOR, ES PRIMO
 }
