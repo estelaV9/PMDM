d6c11069956edc76dbcbaca3c85e567b4abad1b9
estelaV9 <esteladeveg@gmail.com>
2024-10-14 17:46:16 +0200

    crear el menu

--- Tema2_Kotlin/Ejercicios/EjerciciosBasicos/Ejercicio2_AdivinarNumeroOculto/src/main/kotlin/Main.kt
+++ Tema2_Kotlin/Ejercicios/EjerciciosBasicos/Ejercicio2_AdivinarNumeroOculto/src/main/kotlin/Main.kt
@@ -1,7 +1,34 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.util.*
+import kotlin.system.exitProcess
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`) // DECLARAR EN UNA VARIABLE VAL EL SCANNER
+    var opcion:Int = -1
+    var numIntentos:Int
+    var numMaxGenerado:Int
+    print("*********** Adivinar el número oculto ***********")
+    while (opcion != 3) {
+        println("MENU" +
+            "\n1 - Configurar." +
+            "\n2 - Jugar." +
+            "\n3 - Salir.")
+        println("Introducir opcion: ")
+        opcion = scanner.nextInt()
+        when (opcion){
+            1 -> {
+                println("********* CONFIGURAR *********")
+                println("¿Cuántos intentos deseas permitir?")
+                numIntentos = scanner.nextInt()
+                println("¿Cual es el número máximo para generar?")
+                numMaxGenerado = scanner.nextInt()
+            }
+            2 -> {
+
+            }
+            3 -> {
+                println("¡Hasta pronto!")
+                exitProcess(0)
+            }
+        }
+    }
 }
