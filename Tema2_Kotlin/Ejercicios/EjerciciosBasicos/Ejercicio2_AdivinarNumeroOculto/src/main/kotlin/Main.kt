cf73906f2dc8c2b688a5bc2c234719f7604f4984
estelaV9 <esteladeveg@gmail.com>
2024-10-14 17:56:15 +0200

    crear un numero aleatorio

--- Tema2_Kotlin/Ejercicios/EjerciciosBasicos/Ejercicio2_AdivinarNumeroOculto/src/main/kotlin/Main.kt
+++ Tema2_Kotlin/Ejercicios/EjerciciosBasicos/Ejercicio2_AdivinarNumeroOculto/src/main/kotlin/Main.kt
@@ -1,11 +1,13 @@
 import java.util.*
+import kotlin.random.Random // IMPORTAR LA CLASE RANDOM
 import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
     val scanner = Scanner(System.`in`) // DECLARAR EN UNA VARIABLE VAL EL SCANNER
     var opcion:Int = -1
     var numIntentos:Int
-    var numMaxGenerado:Int
+    var numMaxGenerado = 0
+    var numeroAleatorio:Int // GUARDAR EL NUMERO ALEATORIO
     print("*********** Adivinar el número oculto ***********")
     while (opcion != 3) {
         println("MENU" +
@@ -19,11 +21,19 @@ fun main(args: Array<String>) {
                 println("********* CONFIGURAR *********")
                 println("¿Cuántos intentos deseas permitir?")
                 numIntentos = scanner.nextInt()
-                println("¿Cual es el número máximo para generar?")
+                println("¿Cual es el número máximo a adivinar?")
                 numMaxGenerado = scanner.nextInt()
             }
             2 -> {
+                println("******** JUGAR *******")
+                if (numMaxGenerado == 0) {
+                    println("No has introducido ningún numero máximo a adivinar")
+                } else {
+                    // GENERAR UN NUMERO ALEATORIOENTRE 0 Y 9 (EXCLUYENDO EL 10)
+                    numeroAleatorio = Random.nextInt(numMaxGenerado + 1)
 
+                    println("Número aleatorio: $numeroAleatorio")
+                }
             }
             3 -> {
                 println("¡Hasta pronto!")
