bf67e6ec7c8b55d7c6435c0d971c3b5d6f7aa6ee
estelaV9 <esteladeveg@gmail.com>
2024-10-15 09:03:00 +0200

    Crear la clase Vehiculo y hacer instancias de ello

--- Tema2_Kotlin/Ejercicios/EjemplosClases/EjercicioClases/src/main/kotlin/Main.kt
+++ Tema2_Kotlin/Ejercicios/EjemplosClases/EjercicioClases/src/main/kotlin/Main.kt
@@ -3,5 +3,12 @@ package org.example
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
+    // OBJETO CON CONSTRUCTOR PRINCIPAL
+    var miVehiculo = Vehiculo("Renault", "Kadjar", "9843KLS")
 
+    // OBJETO CON NUMERO DE PLAZAS Y MOTOR (constructor secundario)
+    var miVehiculo2 = Vehiculo("Volkswagen", "Tuareg", "2333MMM", 4, "2000cc")
+
+    miVehiculo2.modelo = "nuevo modelo" // LLAMAR AL METODO SET
+    println(miVehiculo.modelo) // LLAMAR AL METODO GET
 }
---
+++ Tema2_Kotlin/Ejercicios/EjemplosClases/EjercicioClases/src/main/kotlin/Vehiculo.kt
@@ -0,0 +1,23 @@
+package org.example
+
+// DEFINICION DE LA CLASE
+class Vehiculo(var marcar:String, var modelo:String, var matricula:String) {
+
+    lateinit var motor:String // LA PROPIEDAD MOTOR SE INICIALIZA MAS TARDE
+
+    // CONSTRUCTOR SECUNDARIO QUE PERMITE CREAR UN Vehiculo CON MAS PARAMETROS
+    constructor(mar:String, mod:String, mat:String, plazas:Int=0, motor:String)
+            : this(mar, mod, mat){ // LLAMA AL CONSTRUCTOR PRIMARIO PARA INICIARLIZAR MARCA, MODELO Y MATRICULA
+                    if(::motor.isInitialized) // NOS DEVUELVE UN VALOR BOOLEAN PARA SABER SI HA SIDO INICIALIZADO O NO
+        this.motor = motor // ASIGNA EL VALOR DEL MOTOR A LA PROPIEDAD
+    }
+
+    fun arrancar(m:String){
+        // VERIFICA SI EL MOTOR NO HA SIDO INICIALIZADO
+        if(!::motor.isInitialized){
+            this.motor=m // ASIGNA EL VALOR DE 'm' A LA PROPIEDAD 'motor'
+        }
+        println("Coche arrancado")
+    } // FUNCION PARA ARRANCAR COCHE
+
+}
