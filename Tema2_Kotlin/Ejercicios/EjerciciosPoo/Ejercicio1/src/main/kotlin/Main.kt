71b5e8877925408a2afa4c8cfe3e4c1b5456039f
estelaV9 <esteladeveg@gmail.com>
2024-10-15 09:58:31 +0200

    crear la clase Dwelling con sus atributos

--- Tema2_Kotlin/Ejercicios/EjerciciosPoo/Ejercicio1/src/main/kotlin/Main.kt
+++ Tema2_Kotlin/Ejercicios/EjerciciosPoo/Ejercicio1/src/main/kotlin/Main.kt
@@ -1,5 +1,8 @@
 package org.example
 
-fun main() {
+import org.example.Model.Dwelling
 
+fun main() {
+    // NO SE PUEDE CREAR UNA INSTANCIA DE Dwelling YA QUE ES UNA CLASE ABSTRACTA
+    // val dwelling = Dwelling()
 }
---
+++ Tema2_Kotlin/Ejercicios/EjerciciosPoo/Ejercicio1/src/main/kotlin/Model/Dwelling.kt
@@ -0,0 +1,15 @@
+package org.example.Model
+
+abstract class Dwelling (private var residents: Int){
+    // CREAR UNA VARIABLE PARA REPRESENTAR EL MATERIAL DE CONSTRUCCION
+    // COMO EL MATERIA NO CAMBIARA, SE USA VAL
+    abstract val buildingMaterial:String
+
+    // CREAR VARIABLE DE CAPACIDAD (cantas personas pueden vivir en ella), TAMPOCO CAMBIA
+    abstract val capacity: Int
+
+    // CREAR VARIABLE DE RESIDENTES (cuantos residentes residen en la vivienda)
+    // SERA UN PARAMETRO QUE SE PASE AL CONSTRUCTOR DE LA CLASE
+    // SE USA DE TIPO PRIVATE PARA SOLO ACCEDER A ELLA DESDE LA PROPIA CLASE
+    // (Dado que la cantidad de personas que viven en una vivienda suele ser información privada)
+}
