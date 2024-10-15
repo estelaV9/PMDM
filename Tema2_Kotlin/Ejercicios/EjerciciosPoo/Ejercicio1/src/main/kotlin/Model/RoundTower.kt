31a28c90339cc39c9cba0e171cbb9803a825e09d
estelaV9 <esteladeveg@gmail.com>
2024-10-15 10:30:49 +0200

    modificar la clase RoundTower

--- Tema2_Kotlin/Ejercicios/EjerciciosPoo/Ejercicio1/src/main/kotlin/Model/RoundTower.kt
+++ Tema2_Kotlin/Ejercicios/EjerciciosPoo/Ejercicio1/src/main/kotlin/Model/RoundTower.kt
@@ -1,7 +1,17 @@
 package org.example.Model
 
-// ES UNA SUBCLASE DE RoundHut
+/*// ES UNA SUBCLASE DE RoundHut
 class RoundTower(residents: Int): RoundHut(residents) {
     override val buildingMaterial = "Stone"
     override val capacity = 4
+}*/
+
+
+// MODIFICAR LA CLASE PARA QUE TENGA VARIOS PISOS Y AJUSTAR LA CAPACIDAD
+// SEGUN SU CANTIDAD
+// SE AÑADE UN NUEVO PARAMETRO PARA RECOGER LA CANTIDAD DE PISOS
+// SE INICIALIZA CON 2 PARA DARLE UN VALOR PREDETERMINADO
+class RoundTower(residents: Int, val floors: Int = 2): RoundHut(residents) {
+    override val buildingMaterial = "Stone"
+    override val capacity = 4 * floors // ACTUALIZAR capacity Y MULTIPLICARLA POR LA CANTIDAD DE PISOS
 }
