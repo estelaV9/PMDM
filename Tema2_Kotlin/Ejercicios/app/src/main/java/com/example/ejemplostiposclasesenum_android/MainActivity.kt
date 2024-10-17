b58c398f0d2730c37b13e6ddd34b9b5c7786c7c9
estelaV9 <esteladeveg@gmail.com>
2024-10-17 10:54:41 +0200

    EJERCICIO TERMINADO: añadir comentarios

--- Tema2_Kotlin/Ejercicios/app/src/main/java/com/example/ejemplostiposclasesenum_android/MainActivity.kt
+++ Tema2_Kotlin/Ejercicios/app/src/main/java/com/example/ejemplostiposclasesenum_android/MainActivity.kt
@@ -13,13 +13,16 @@ import androidx.constraintlayout.widget.ConstraintLayout
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main) // ESTABLECE EL LAYOUT DE LA ACTIVIDAD
+
         // OBTENER LA REFERENCA DEL RADIO GROUP
-        var radioGrupo_Colores = findViewById<RadioGroup>(R.id.radio_group_color)
+        var radioGrupo_Colores =
+            findViewById<RadioGroup>(R.id.radio_group_color) // ENLAZA EL RADIOGROUP AL CODIGO
 
         /***************************** UNA FORMA *******************************/
         // AGREGAR UN ESCUCHADOR PARA EL COMPONENTE DEL RADIO GROUP
         radioGrupo_Colores.setOnCheckedChangeListener((object : RadioGroup.OnCheckedChangeListener {
+            // OBTENER LAS REFERENCIAS A CADA RADIOBUTTON
             var radio_verde = findViewById<RadioButton>(R.id.radioVerde)
             var radio_azul = findViewById<RadioButton>(R.id.radioAzul)
             var radio_rojo = findViewById<RadioButton>(R.id.radioRojo)
@@ -27,8 +30,9 @@ class MainActivity : AppCompatActivity() {
             var miLayout =
                 findViewById<ConstraintLayout>(R.id.main) // CONTENEDOR DE TODA LA PANTALLA
 
-            @RequiresApi(Build.VERSION_CODES.O)
+            @RequiresApi(Build.VERSION_CODES.O) // REQUIERE UNA API MINIMA PARA USAR 'toArgb()'
             override fun onCheckedChanged(p0: RadioGroup?, p1: Int) {
+                // CAMBIAR DEL COLOR DEL FONDO SEGUN EL RADIOBUTTON SELECCIONADO
                 when (p1) {
                     radio_rojo.id -> // CAMBIAR EL COLOR A ROJO
                         miLayout.setBackgroundColor(Vcolor.ROJO.retornar_Color().toArgb())
@@ -46,40 +50,39 @@ class MainActivity : AppCompatActivity() {
         }))
 
         /***************************** SEGUNDA FORMA *******************************/
-        /* //Creamos un setOnCheckedChangeListener que nos pedirá el radioGroup que hemos guardado
-        //Anteriormente y checkedId que es variable definida por android studio
+        /*  // CREAMOS UN SETONCHECKEDCHANListener QUE NOS PEDIRÁ EL RADIOGROUP QUE HEMOS GUARDADO
+        // ANTERIORMENTE Y CHECKEDID QUE ES VARIABLE DEFINIDA POR ANDROID STUDIO
         radiogroup.setOnCheckedChangeListener{radioGroup,checkedId ->
-            //Coges el id del panel principal que quieres cambiar de color, Esta asi en el xml--> android:id="@+id/main"
+            // COGES EL ID DEL PANEL PRINCIPAL QUE QUIERES CAMBIAR DE COLOR, ESTÁ ASÍ EN EL XML --> android:id="@+id/main"
             val mainLayout = findViewById<ConstraintLayout>(R.id.main)
 
-            when (checkedId) {
-                R.id.RbRojo -> {
-                    val colorRojo:Int = Vcolor.ROJO.retornarColor()
-                    mainLayout.setBackgroundColor(colorRojo)
+            when (checkedId) { // VERIFICAMOS CUAL RADIOBUTTON FUE SELECCIONADO
+                R.id.RbRojo -> { // SI SE SELECCIONA ROJO
+                    val colorRojo:Int = Vcolor.ROJO.retornarColor() // OBTENEMOS EL COLOR ROJO
+                    mainLayout.setBackgroundColor(colorRojo) // CAMBIAMOS EL FONDO AL COLOR ROJO
                 }
-                R.id.RbAzul -> {
-                    val colorAzul:Int = Vcolor.AZUL.retornarColor()
-                    mainLayout.setBackgroundColor(colorAzul)
+                R.id.RbAzul -> { // SI SE SELECCIONA AZUL
+                    val colorAzul:Int = Vcolor.AZUL.retornarColor() // OBTENEMOS EL COLOR AZUL
+                    mainLayout.setBackgroundColor(colorAzul) // CAMBIAMOS EL FONDO AL COLOR AZUL
                 }
-                R.id.RbVerde -> {
-                    val colorVerde:Int = Vcolor.VERDE.retornarColor()
-                    mainLayout.setBackgroundColor(colorVerde)
+                R.id.RbVerde -> { // SI SE SELECCIONA VERDE
+                    val colorVerde:Int = Vcolor.VERDE.retornarColor() // OBTENEMOS EL COLOR VERDE
+                    mainLayout.setBackgroundColor(colorVerde) // CAMBIAMOS EL FONDO AL COLOR VERDE
                 }
-                R.id.RbNegro -> {
-                    val colorNegro:Int = Vcolor.NEGRO.retornarColor()
-                    mainLayout.setBackgroundColor(colorNegro)
+                R.id.RbNegro -> { // SI SE SELECCIONA NEGRO
+                    val colorNegro:Int = Vcolor.NEGRO.retornarColor() // OBTENEMOS EL COLOR NEGRO
+                    mainLayout.setBackgroundColor(colorNegro) // CAMBIAMOS EL FONDO AL COLOR NEGRO
                 }
-
             }
         }
 
         // CAMBIAR LA FUNCION PARA QUE DEVUELVA DIRECTAMENTE UN VALOR PREDEFINIDO
-        fun retornarColor(): Int {
-            return when(this) {
-                ROJO->Color.RED
-                AZUL->Color.BLUE
-                VERDE->Color.GREEN
-                NEGRO->Color.BLACK
+        fun retornarColor(): Int { // FUNCIÓN PARA RETORNAR EL COLOR
+            return when(this) { // VERIFICAR CUAL ES EL COLOR ACTUAL
+                ROJO->Color.RED // RETORNAR COLOR ROJO
+                AZUL->Color.BLUE // RETORNAR COLOR AZUL
+                VERDE->Color.GREEN // RETORNAR COLOR VERDE
+                NEGRO->Color.BLACK // RETORNAR COLOR NEGRO
             }
         } */
     }
