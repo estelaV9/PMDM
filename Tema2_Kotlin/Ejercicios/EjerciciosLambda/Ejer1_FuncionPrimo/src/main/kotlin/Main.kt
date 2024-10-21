1a8613197745b29da12dce932d5435dd646312e8
estelaV9 <esteladeveg@gmail.com>
2024-10-21 16:24:13 +0200

    crear ejercicio 1 de la tanda de ejercicio lambdas

---
+++ Tema2_Kotlin/Ejercicios/EjerciciosLambda/Ejer1_FuncionPrimo/src/main/kotlin/Main.kt
@@ -0,0 +1,6 @@
+/* Ejercicio 1:  Diseña una función con sintaxis lambda que devuelva (true o false) si
+un número es primo. Para probarla puede asignar la función a una variable  */
+
+fun main(args: Array<String>) {
+
+}
