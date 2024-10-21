c3a92aa2bf32898806c2d6b60ce03e59499321c0
estelaV9 <esteladeveg@gmail.com>
2024-10-21 16:52:18 +0200

    crear ejercicio 2 de numeros magicos con funciones lambdas

---
+++ Tema2_Kotlin/Ejercicios/EjerciciosLambda/Ejer2_NumerosMagicos/src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+/* Ejercicio 2: Diseña una función con sintaxis lambda que devuelva(true o false) si
+un número es mágico.
+Los números mágicos son aquellos que al elevarlos al cubo
+y sumamos sus cifras nos dan el mismo resultado. Para probarla puede asignar la
+función a una variable  */
+fun main(args: Array<String>) {
+
+}
