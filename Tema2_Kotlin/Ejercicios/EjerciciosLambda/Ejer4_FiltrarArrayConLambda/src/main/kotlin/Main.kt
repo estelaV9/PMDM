5d5310862ffe4b525bdeead649206c4600de17ce
estelaV9 <esteladeveg@gmail.com>
2024-10-21 18:11:50 +0200

    crear ejercicio 4: valores filtrados segun la funcion que se le pase

---
+++ Tema2_Kotlin/Ejercicios/EjerciciosLambda/Ejer4_FiltrarArrayConLambda/src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+/* Ejercicio 4.- Diseñe una función de orden superior que reciba como parámetro un
+array de enteros y una función (en forma de expresión lambda) y devuelva un array
+de enteros con los valores filtrados según la función que se le pase como segundo
+argumento. La función que se le pasa como 2º parámetro recibe un número entero
+y devuelve un boolean. */
+fun main(args: Array<String>) {
+
+}
