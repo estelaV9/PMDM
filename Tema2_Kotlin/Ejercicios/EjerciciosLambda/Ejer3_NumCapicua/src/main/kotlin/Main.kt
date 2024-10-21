e86e946dd38520ce9a7fc7b8f1c3c8542900c9de
estelaV9 <esteladeveg@gmail.com>
2024-10-21 17:19:57 +0200

    crear el ejercicio 3 del numero capicua

---
+++ Tema2_Kotlin/Ejercicios/EjerciciosLambda/Ejer3_NumCapicua/src/main/kotlin/Main.kt
@@ -0,0 +1,6 @@
+/* Ejercicio 3: Diseña una función con sintaxis lambda que devuelva (true o false) si
+un número es capicúa (se lee igual de izquierda a derecha que de derecha a
+izquierda). Para probarla puede asignar la función a una variable */
+fun main(args: Array<String>) {
+
+}
