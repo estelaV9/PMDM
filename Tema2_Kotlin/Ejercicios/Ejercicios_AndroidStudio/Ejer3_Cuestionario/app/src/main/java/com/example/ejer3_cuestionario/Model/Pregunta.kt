15360311870b384b696c849557934044f0842170
estelaV9 <esteladeveg@gmail.com>
2024-10-25 20:21:06 +0200

    crear los atributos y funciones de la clase pregunta

--- Tema2_Kotlin/Ejercicios/Ejercicios_AndroidStudio/Ejer3_Cuestionario/app/src/main/java/com/example/ejer3_cuestionario/Model/Pregunta.kt
+++ Tema2_Kotlin/Ejercicios/Ejercicios_AndroidStudio/Ejer3_Cuestionario/app/src/main/java/com/example/ejer3_cuestionario/Model/Pregunta.kt
@@ -1,4 +1,24 @@
-package com.example.ejer3_cuestionario.Model;
+package com.example.ejer3_cuestionario.Model
 
-public class Pregunta {
+class Pregunta(pregunta: String) {
+
+    enum class TipoDificultad {
+        FACIL, MEDIA, ALTA
+    }
+
+    var pregunta: String = ""
+    lateinit var dificultad: TipoDificultad
+    var respuesta: String = ""
+
+    constructor(pregunta: String, dificultad: TipoDificultad, respuesta: String)
+            : this(pregunta) {
+        this.pregunta = pregunta
+        this.dificultad = dificultad
+        this.respuesta = respuesta
+    }
+
+
+    fun corregirRespuesta (respuesta: String): Boolean {
+        return respuesta == this.respuesta
+    }
 }
