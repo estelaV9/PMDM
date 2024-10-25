7da388c5dc75e58cd4de8fd70df4b30ecc7aa23f
estelaV9 <esteladeveg@gmail.com>
2024-10-25 19:05:29 +0200

    EJERCICIO TERMINADO: crear la funcion y añadir el listener

--- Tema2_Kotlin/Ejercicios/Ejercicios_AndroidStudio/Ejer2_TextFieldPorCaracter/app/src/main/java/com/example/ejer2_textfieldporcaracter/MainActivity.kt
+++ Tema2_Kotlin/Ejercicios/Ejercicios_AndroidStudio/Ejer2_TextFieldPorCaracter/app/src/main/java/com/example/ejer2_textfieldporcaracter/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.ejer2_textfieldporcaracter
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +15,35 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val editText = findViewById<EditText>(R.id.caracteresEdit)
+        val textField = findViewById<TextView>(R.id.textField)
+
+        // Agrega un TextWatcher para detectar cambios en el texto del EditText
+        editText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // No se necesita implementación en este caso
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // No se necesita implementación en este caso
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // Llama a la función caracteresIntroducidos para obtener la cadena de guiones y la muestra en el TextView
+                val guiones = caracteresIntroducidos(s.toString())
+                textField.text = guiones
+            }
+        })
+    }
+
+    fun <T> caracteresIntroducidos(caracteres: T): String {
+        val numCaracteres = caracteres.toString()
+        var numGuiones = "";
+        // SE USA UNTIL PARA EVITAR EL INDICE FUERA DEL RANGO
+        for (i: Int in 0 until numCaracteres.length) {
+            numGuiones += "_"
         }
+        return numGuiones;
     }
 }
