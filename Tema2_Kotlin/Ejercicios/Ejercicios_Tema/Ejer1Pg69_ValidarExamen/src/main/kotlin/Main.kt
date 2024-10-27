39a884a64a0151705d75a0bf85334345352be014
estelaV9 <esteladeveg@gmail.com>
2024-10-27 16:36:55 +0100

    EJERCICIO TERMINADO: crear las listas con las preguntas y respuestas y mostrar la puntuacion obtenida

--- Tema2_Kotlin/Ejercicios/Ejercicios_Tema/Ejer1Pg69_ValidarExamen/src/main/kotlin/Main.kt
+++ Tema2_Kotlin/Ejercicios/Ejercicios_Tema/Ejer1Pg69_ValidarExamen/src/main/kotlin/Main.kt
@@ -1,3 +1,30 @@
-fun main(args: Array<String>) {
+import java.util.Scanner
 
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+    var sumaPuntuacion = 0 // ALMACENA LA PUNTUACION ACUMULADA DEL EXAMEN
+
+    // CREAR UNA LISTA CON LAS PREGUNTAS DEL EXAMEN
+    val preguntasExamen: List<String> = listOf(
+        "¿Cuál es la capital de Francia?",
+        "¿Cuál es la respuesta correcta? a) Opción1 b) Opción2 c) Opción3 d) Opción4",
+        "La Tierra es plana: verdadero o falso (true/false)",
+        "¿Cuánto es 5 x 5?"
+    )
+
+    // LISTA CON LAS RESPUESTAS DE LAS PREGUNTAS DEL EXAMEN
+    val respuestasExamenes: List<String> = listOf(
+        "paris", "b", "false", "25"
+    )
+
+    for (i in preguntasExamen.indices) {
+        println(preguntasExamen[i]) // SE MUESTRA LA PREGUNTA
+        val respuesta = scanner.nextLine().lowercase() // SE INTRODUCE LA RESPUESTA EN MINUSCULAS
+
+        if (respuesta == respuestasExamenes[i]) {
+            sumaPuntuacion += 1 // SUMA UN PUNTO A LA PUNTUACION TOTAL
+        } // SE COMPARA LA RESPUESTA INTRODUCIDA CON LA RESPUESTA DE LA PREGUNTA
+    } // RECORRER LOS INDICES DE LAS PREGUNTAS DEL EXAMEN
+
+    println("Tu puntuacion en este examen es: $sumaPuntuacion")
 }
