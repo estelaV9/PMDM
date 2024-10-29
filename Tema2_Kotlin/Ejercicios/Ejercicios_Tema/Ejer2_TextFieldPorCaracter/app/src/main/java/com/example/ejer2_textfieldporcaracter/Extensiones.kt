737effa35d0944614028e4e55674fd86d5be02af
Estela de Vega <130656156+estelaV9@users.noreply.github.com>
2024-10-29 09:57:27 +0100

    poner comentario en extensiones

--- Tema2_Kotlin/Ejercicios/Ejercicios_Tema/Ejer2_TextFieldPorCaracter/app/src/main/java/com/example/ejer2_textfieldporcaracter/Extensiones.kt
+++ Tema2_Kotlin/Ejercicios/Ejercicios_Tema/Ejer2_TextFieldPorCaracter/app/src/main/java/com/example/ejer2_textfieldporcaracter/Extensiones.kt
@@ -9,52 +9,20 @@ import android.widget.Toast
 import androidx.annotation.ColorRes
 import androidx.core.content.ContextCompat
 
-// Función para comprobar si un objeto es null
-/*fun Any?.esnulo(): Boolean {
-    return this == null
-}
-// Ejercicio1.- Es posible definir esta función con menos código?
 
-// Funcion para simplificar un Toast, podrá ser invocada desde cualquier Activity
-fun Activity.mostrar_mensaje(mensaje: String, duracion: Int = Toast.LENGTH_SHORT) {
-    Toast.makeText(this, mensaje, duracion).show()
-}
+/*funcion de extension = crear una clase que herede de otra clase y definir dentro otra funcion
 
-/**
- * Funcion para obtener un color a partir de un valor de recurso, podrá ser invocada desde
- * cualquier activity. Se etiqueta con la anotación @ColorRes (ver anotaciones de recursos en
- * https://developer.android.com/studio/write/annotations?hl=es-419) para impedir en tiempo de
- * compilación que se introduzca un valor que no corresponde con un valor de recurso de color
- * @param color_res Parametro que representa un recurso de color
- * @return Un entero que representa el color en formato AARRGGBB
- *
- */
+Se crea dentro de la clase EditText una funcion despues_cambio_texto
+se le pasa una funcion le paso como parametro otra funcion le tengo que decir como es esa funcion : esa funcion tiene x parametros y va a devolver algo
 
-// desde cualquier activity
-fun Activity.get_color(@ColorRes color_res: Int): Int {
-    return ContextCompat.getColor(this, color_res)
-}
+(String, Int..) -> param
+Util -> lo que devuelve / retorna
 
-// Ejercicio 2.- Podemos definir la función de otra forma más sencilla?
-/**
- * En este caso una función que permite cargar imagenes a partir de una
- * url haciendo uso de la libreria Glide,sera necesario importarla en gradle
- * Glide es una libreria muy potente para cargar recursos desde URL's
- * (https://bumptech.github.io/glide/doc/download-setup.html)
- * Configuración e instalacion de libreria y en los ficheros gradle:
- * https://bumptech.github.io/glide/doc/download-setup.html
- */
-fun ImageView.cargarImagen(url: String) {
-    if (url.isNotEmpty()) {
-        // Cargamos la imagen, this.context representa el contexto de
-        // la imagen, es decir, de que Activity depende la vista
-        // ImageView es un contenedor de imagenes, into dice que se cargue
-        // la imagen de la URL en este contenedor.
-        Glide.with(this.context).load(url).into(this)
-    }
-}*/
-
+En este caso, no devuelve nada, le paso una cadena como parametro
+La cadena que le pase es la descri`cion de lo que esta escribiendo en el editText
+*/
 fun EditText.despues_cambio_texto(listener: (String) -> Unit) {
+    //  Dentro de la funicon de extencion se implementa un escuchador al editText el this se referencia al objeto que invoque esa funcion
     this.addTextChangedListener(object : TextWatcher {
         override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
             //En este caso no se hace nada
@@ -65,6 +33,7 @@ fun EditText.despues_cambio_texto(listener: (String) -> Unit) {
         }
 
         override fun afterTextChanged(s: Editable?) {
+            //Invoca a la funcion pasada como parametro.
             listener(s.toString())
         }
     })
