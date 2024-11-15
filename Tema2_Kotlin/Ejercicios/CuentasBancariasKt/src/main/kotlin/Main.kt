3ae1727a5da1fc3b48cdd76d67255a066fc6ae28
estelaV9 <esteladeveg@gmail.com>
2024-11-15 19:57:21 +0100

    crear el menu del main

--- Tema2_Kotlin/Ejercicios/CuentasBancariasKt/src/main/kotlin/Main.kt
+++ Tema2_Kotlin/Ejercicios/CuentasBancariasKt/src/main/kotlin/Main.kt
@@ -1,7 +1,57 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.util.Scanner
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    var option: Int = -1
+    val scanner = Scanner(System.`in`)
+    println("Bienvenido a BankPal!")
+    try {
+        do {
+            println("\n1-Abrir una nueva cuenta")
+            println("\n2-Ver un listado de las cuentas disponibles")
+            println("\n3-Obtener los datos de una cuenta concreta")
+            println("\n4-Realizar un ingreso en una cuenta.")
+            println("\n5-Retirar efectivo de una cuenta. ")
+            println("\n6-Consultar el saldo actual de una cuenta.")
+            println("\n7-Salir")
+
+            println("Elige una opcion: ")
+            option = scanner.nextInt()
+
+            when (option) {
+                1 -> {
+
+                }
+
+                2 -> {
+
+                }
+
+                3 -> {
+
+                }
+
+                4 -> {
+
+                }
+
+                5 -> {
+
+                }
+
+                6 -> {
+
+                }
+
+                7 -> {
+                    println("Vuelva pronto!")
+                    System.exit(0)
+                }
+            }
+
+
+        } while (option != 7)
+    } catch (e: Exception) {
+        println("Ha ocurrido un error: $e")
+    }
+    println("adios!!")
 }
