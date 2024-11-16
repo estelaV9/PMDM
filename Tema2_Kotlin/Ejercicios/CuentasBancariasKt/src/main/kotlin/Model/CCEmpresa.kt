b9a8164dc1b8caced6e1aa5ed0608d294f028178
estelaV9 <esteladeveg@gmail.com>
2024-11-16 10:34:29 +0100

    crear la clase de cuenta corriente de empresa

---
+++ Tema2_Kotlin/Ejercicios/CuentasBancariasKt/src/main/kotlin/Model/CCEmpresa.kt
@@ -0,0 +1,29 @@
+package Model
+
+class CCEmpresa(
+    titular: Persona,
+    saldo: Double,
+    iban: String,
+    private var maxDescubierto: Int,
+    private var tipoInteresDescubierto: Int
+) : CuentaCorriente(titular, saldo, iban) {
+
+    init {
+        this.titular = titular
+        this.saldo = saldo
+        this.iban = iban
+    }
+
+    override fun toString(): String {
+        return """
+        Cuenta Corriente de Empresa:
+        -----------------
+        Titular         : $titular
+        Saldo           : $saldo
+        IBAN            : $iban
+        Lista Entidades : ${listaEntidades.joinToString {", " }}
+        Maximo Descubierto : $maxDescubierto
+        Tipo Interes Descubierto : $tipoInteresDescubierto
+    """.trimIndent()
+    }
+}
