f91ecfcd41e4f3592dcae313b5ea6120fbdfc640
estelaV9 <esteladeveg@gmail.com>
2024-11-16 20:52:11 +0100

    añadir un get al iban en la clase CuentaBancaria

--- Tema2_Kotlin/Ejercicios/CuentasBancariasKt/src/main/kotlin/Model/CuentaBancaria.kt
+++ Tema2_Kotlin/Ejercicios/CuentasBancariasKt/src/main/kotlin/Model/CuentaBancaria.kt
@@ -3,6 +3,10 @@ package Model
 abstract class CuentaBancaria(
     protected var titular: Persona,
     protected var saldo: Double,
-    protected var iban: String
+    protected var _iban: String
 ) : Interface {
+
+    // SE AÑADE UN GETTER
+    val iban: String
+        get() = _iban
 }
