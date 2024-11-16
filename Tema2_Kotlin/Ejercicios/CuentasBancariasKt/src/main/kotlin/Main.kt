2296f0e7afd183aae0bf8e3eba386f3704752016
estelaV9 <esteladeveg@gmail.com>
2024-11-16 11:55:12 +0100

    hacer la primera opcion y crear una cuenta bancaria

--- Tema2_Kotlin/Ejercicios/CuentasBancariasKt/src/main/kotlin/Main.kt
+++ Tema2_Kotlin/Ejercicios/CuentasBancariasKt/src/main/kotlin/Main.kt
@@ -1,3 +1,4 @@
+import Model.*
 import java.util.Scanner
 
 fun main(args: Array<String>) {
@@ -5,25 +6,79 @@ fun main(args: Array<String>) {
     val scanner = Scanner(System.`in`)
     println("Bienvenido a BankPal!")
     try {
+        val cuentas: MutableList<CuentaBancaria> = mutableListOf()
         do {
-            println("\n1-Abrir una nueva cuenta")
-            println("\n2-Ver un listado de las cuentas disponibles")
-            println("\n3-Obtener los datos de una cuenta concreta")
-            println("\n4-Realizar un ingreso en una cuenta.")
-            println("\n5-Retirar efectivo de una cuenta. ")
-            println("\n6-Consultar el saldo actual de una cuenta.")
-            println("\n7-Salir")
+            println("1-Abrir una nueva cuenta")
+            println("2-Ver un listado de las cuentas disponibles")
+            println("3-Obtener los datos de una cuenta concreta")
+            println("4-Realizar un ingreso en una cuenta.")
+            println("5-Retirar efectivo de una cuenta. ")
+            println("6-Consultar el saldo actual de una cuenta.")
+            println("7-Salir")
 
             println("Elige una opcion: ")
             option = scanner.nextInt()
 
             when (option) {
                 1 -> {
+                    println("*********** NUEVA CUENTA ************")
+                    println("Introduzca los datos personales")
+                    print("Nombre: ")
+                    val nombre = scanner.next()
+                    print("Apellidos: ")
+                    val apellidos = scanner.next()
+                    print("DNI: ")
+                    val dni = scanner.next()
+                    val titular = Persona(nombre, apellidos, dni)
+                    println(
+                        "Tipo de cuenta: " +
+                                "\n1 - Cuenta de Ahorro" +
+                                "\n2 - Cuenta Corriente personal" +
+                                "\n3 - Cuenta Corriente de Empresa"
+                    )
+                    val tipoCuentaNum = scanner.nextInt()
+                    println("Saldo inicial: ")
+                    val saldoInicial = scanner.nextDouble()
 
+                    println("Numero de Cuenta")
+                    val iban = scanner.next()
+                    when (tipoCuentaNum) {
+                        1 -> {
+                            println("Tipo de interes: ")
+                            val tipoInteres = scanner.nextDouble()
+                            val cuentaAhorro:CuentaBancaria = CuentaAhorro(titular, saldoInicial, iban, tipoInteres)
+                            cuentas.add(cuentaAhorro)
+                        }
+
+                        2 -> {
+                            println("Comision Mantenimiento: ")
+                            val comisionMantenimiento = scanner.nextInt()
+                            val ccpersonal:CuentaBancaria = CCPersonal(titular, saldoInicial, iban, comisionMantenimiento)
+                            cuentas.add(ccpersonal)
+                        }
+
+                        3 -> {
+                            println("Maximo Descubierto Permitido: ")
+                            val maxDescubierto = scanner.nextInt()
+                            println("Tipo interes por descubierto: ")
+                            val tipoInteres = scanner.nextDouble()
+                            println("ComisionFija: ")
+                            val comisionFija = scanner.nextDouble()
+                            val ccEmpresa:CuentaBancaria =
+                                CCEmpresa(titular, saldoInicial, iban, maxDescubierto, tipoInteres, comisionFija)
+                            cuentas.add(ccEmpresa)
+                        }
+
+                        else -> println("Numero no valido")
+                    }
                 }
 
                 2 -> {
-
+                    println("CUENTAS BANCARIAS")
+                   for (c in cuentas){
+                       println(c.devolverInfo())
+                       println()
+                    }
                 }
 
                 3 -> {
