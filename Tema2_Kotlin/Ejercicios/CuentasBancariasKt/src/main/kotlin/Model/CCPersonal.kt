278cdd9e3517f37d398ee4c7479c2a21e79f4692
estelaV9 <esteladeveg@gmail.com>
2024-11-16 10:28:49 +0100

    crear la clase de cuenta corriente personal

---
+++ Tema2_Kotlin/Ejercicios/CuentasBancariasKt/src/main/kotlin/Model/CCPersonal.kt
@@ -0,0 +1,28 @@
+package Model
+
+class CCPersonal(
+    titular: Persona,
+    saldo: Double,
+    iban: String,
+    private var comisionMantenimiento: Int
+) : CuentaCorriente(titular, saldo, iban) {
+
+    init {
+        this.titular = titular
+        this.saldo = saldo
+        this.iban = iban
+    }
+
+    override fun toString(): String {
+        return """
+        Cuenta Corriente Personal:
+        -----------------
+        Titular         : $titular
+        Saldo           : $saldo
+        IBAN            : $iban
+        Lista Entidades : ${listaEntidades.joinToString {", " }}
+        Comision de Mantenimiento : $comisionMantenimiento
+    """.trimIndent()
+    }
+
+}
