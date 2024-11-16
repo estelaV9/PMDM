47183b13a5a3d4182e42c378d59fbe34c401f72e
estelaV9 <esteladeveg@gmail.com>
2024-11-16 10:27:23 +0100

    cambiar cuentacorriente a open y su atributo de listaEntidades ponerle protected para que las subclases puedan acceder a el

--- Tema2_Kotlin/Ejercicios/CuentasBancariasKt/src/main/kotlin/Model/CuentaCorriente.kt
+++ Tema2_Kotlin/Ejercicios/CuentasBancariasKt/src/main/kotlin/Model/CuentaCorriente.kt
@@ -1,11 +1,13 @@
 package Model
 
-class CuentaCorriente(
+open class CuentaCorriente(
     titular: Persona,
     saldo: Double,
     iban: String,
-    private var listaEntidades: Array<String>
 ) : CuentaBancaria(titular, saldo, iban) {
+
+    // PARA QUE LAS DEMAS CLASES PUEDAN ACCEDER A ESTE ATRIBUTO SE PONE PROTECTED
+    protected var listaEntidades: Array<String>
 
     init {
         this.titular = titular
