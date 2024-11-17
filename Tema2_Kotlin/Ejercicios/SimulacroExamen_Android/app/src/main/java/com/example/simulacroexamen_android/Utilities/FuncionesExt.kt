08e8db6947390a0537186fe6a8586fc42b2c4838
estelaV9 <esteladeveg@gmail.com>
2024-11-17 22:06:09 +0100

    añadir la funcion de extension para filtrar

--- Tema2_Kotlin/Ejercicios/SimulacroExamen_Android/app/src/main/java/com/example/simulacroexamen_android/Utilities/FuncionesExt.kt
+++ Tema2_Kotlin/Ejercicios/SimulacroExamen_Android/app/src/main/java/com/example/simulacroexamen_android/Utilities/FuncionesExt.kt
@@ -1,5 +1,6 @@
 package com.example.simulacroexamen_android.Utilities
 
+import Model.Actividad
 import android.content.Context
 import android.widget.Toast
 
@@ -9,4 +10,12 @@ fun Context.mensaje(mensaje:String, duracion:Int = Toast.LENGTH_LONG){ // POR DE
         mensaje,
         duracion
     ).show()
+}
+
+// FUNCION DE EXTENSION Y DE ALTO NIVEL QUE PERMITA FILTRAR UN ARRAYLIST DE ACTIVIDADES
+// EN FUNCION DE UNA LAMBDA COMO PARAMERTO.
+// LA LAMBDA TIENE COMO PARAMETRO UNA ACTIVIDAD Y RETORNA UN BOOLEAN
+// RETORNA UN ARRAYLIST DE ACTIVIDADES
+fun ArrayList<Actividad>.filtrarPor(condicion: (Actividad) -> Boolean): ArrayList<Actividad> {
+    return ArrayList(this.filter(condicion))
 }
