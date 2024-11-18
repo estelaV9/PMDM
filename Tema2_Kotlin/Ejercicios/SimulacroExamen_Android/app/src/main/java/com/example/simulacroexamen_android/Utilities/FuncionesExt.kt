39e6bd453d492518c2720807025a2a021b10b07b
estelaV9 <esteladeveg@gmail.com>
2024-11-18 22:22:38 +0100

    crear la funcion de extension filtrarPagosPor

--- Tema2_Kotlin/Ejercicios/SimulacroExamen_Android/app/src/main/java/com/example/simulacroexamen_android/Utilities/FuncionesExt.kt
+++ Tema2_Kotlin/Ejercicios/SimulacroExamen_Android/app/src/main/java/com/example/simulacroexamen_android/Utilities/FuncionesExt.kt
@@ -2,9 +2,12 @@ package com.example.simulacroexamen_android.Utilities
 
 import Model.Actividad
 import android.content.Context
+import android.os.Build
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import com.example.simulacroexamen_android.Model.Pago
 
-fun Context.mensaje(mensaje:String, duracion:Int = Toast.LENGTH_LONG){ // POR DEFECTO ES LONG
+fun Context.mensaje(mensaje: String, duracion: Int = Toast.LENGTH_LONG) { // POR DEFECTO ES LONG
     Toast.makeText(
         this,
         mensaje,
@@ -18,4 +21,11 @@ fun Context.mensaje(mensaje:String, duracion:Int = Toast.LENGTH_LONG){ // POR DE
 // RETORNA UN ARRAYLIST DE ACTIVIDADES
 fun ArrayList<Actividad>.filtrarPor(condicion: (Actividad) -> Boolean): ArrayList<Actividad> {
     return ArrayList(this.filter(condicion))
+}
+
+// DEFINIR UNA FUNCION DE EXTENSION SOBRE ARRAYLIST DE PAGOS QUE RECIBA UNA
+// LAMBDA CON UN PARAMETRO PAGO Y DEVUELVA UN DOUBLE
+fun ArrayList<Pago>.filtrarPagosPor(condicion: (Pago) -> Boolean): Double {
+    // FILTRA LOS PAGOS QUE CUMPLEN LA CONDICIÓN Y SUMA SUS CANTIDADE
+    return this.filter(condicion).sumOf { it.cantidad }
 }
