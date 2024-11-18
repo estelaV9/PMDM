02744ba3d546ba76bf59afde93f26bc2b47accfd
estelaV9 <esteladeveg@gmail.com>
2024-11-18 22:22:56 +0100

    añadirle codigo a la clase pago

--- Tema2_Kotlin/Ejercicios/SimulacroExamen_Android/app/src/main/java/com/example/simulacroexamen_android/Model/Pago.kt
+++ Tema2_Kotlin/Ejercicios/SimulacroExamen_Android/app/src/main/java/com/example/simulacroexamen_android/Model/Pago.kt
@@ -1,6 +1,25 @@
 package com.example.simulacroexamen_android.Model
 
 import Model.Actividad
+import android.content.Context
+import com.example.simulacroexamen_android.Utilities.mensaje
+import java.time.LocalDate
 
-class Pago : Actividad{
+class Pago(
+    var cantidad: Double,
+    var fechaPago: LocalDate,
+    var metodoPago: String,
+    nombre: String,
+    completada: Boolean
+) : Actividad(nombre, completada) {
+
+    fun procesarPago(context: Context): Unit {
+        marcarComoCompletada() // LLAMAR AL METODO CLASE SUPERIOR
+        // MOSTRAR UN MENSAJE (Toast), PAGO PROCESADO
+        context.mensaje("Pago procesado")
+    } // LLAMAR AL METODO MARCARCOMOCOMLETADA Y MOSTRAR UN MENSAJE
+
+    override fun mostrarDetalle(): String {
+        return "Pago: Cantidad: $cantidad, Fecha de pago: $fechaPago, Método: $metodoPago]"
+    } // RETORNA STRING CON LA DESCRIPCION DE UN PAGO
 }
