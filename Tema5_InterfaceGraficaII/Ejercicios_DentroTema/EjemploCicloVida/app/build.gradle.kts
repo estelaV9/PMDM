8db298c4e2621e69db07d47136017935fe9342f1
estelaV9 <esteladeveg@gmail.com>
2024-11-28 10:04:03 +0100

    Terminar el ejemplo de ciclo de vida de una actividad

--- Tema5_InterfaceGraficaII/Ejercicios_DentroTema/EjemploCicloVida/app/build.gradle.kts
+++ Tema5_InterfaceGraficaII/Ejercicios_DentroTema/EjemploCicloVida/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.ejemplociclovida"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.ejemplociclovida"
