095976338eccc582e3c39cdfe565bc244b76ba3a
estelaV9 <esteladeveg@gmail.com>
2024-11-30 16:20:45 +0100

    crear una intencion que cuando pulse el boton vaya a mi github

--- Tema5_InterfaceGraficaII/Intenciones/app/src/main/java/com/example/intenciones/MainActivity.kt
+++ Tema5_InterfaceGraficaII/Intenciones/app/src/main/java/com/example/intenciones/MainActivity.kt
@@ -1,20 +1,26 @@
 package com.example.intenciones
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.intenciones.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var mibinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        mibinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mibinding.root)
+
+        mibinding.githubBtt.setOnClickListener {
+            // CREAR UN INTENT PARA ABRIR LA URL
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/estelaV9"))
+
+            // INICIAR LA ACTIVIDAD CON EL INTENT
+            startActivity(intent)
         }
     }
 }
