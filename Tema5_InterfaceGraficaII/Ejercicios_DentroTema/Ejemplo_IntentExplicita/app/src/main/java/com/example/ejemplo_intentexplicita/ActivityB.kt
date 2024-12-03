726b5218a3ef6d8c71c5922fd63930f24cf9da32
estelaV9 <esteladeveg@gmail.com>
2024-12-03 11:07:11 +0100

    añadir para devolver datos

--- Tema5_InterfaceGraficaII/Ejercicios_DentroTema/Ejemplo_IntentExplicita/app/src/main/java/com/example/ejemplo_intentexplicita/ActivityB.kt
+++ Tema5_InterfaceGraficaII/Ejercicios_DentroTema/Ejemplo_IntentExplicita/app/src/main/java/com/example/ejemplo_intentexplicita/ActivityB.kt
@@ -1,6 +1,8 @@
 package com.example.ejemplo_intentexplicita
 
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -35,6 +37,18 @@ class ActivityB : AppCompatActivity() {
 
         // LISTENER AL BOTON
         mibinding.finalizarActBBtt.setOnClickListener {
+            // CREAMOS UN INTENT QUE ESTA VINCULADO CON LA MAINACTIVITY QUE ME SIRVE PARA DEDOLVER DATOSS
+            val miIntent = Intent()
+            // CREO UN BUNDLE PARA METER LOS DATOS
+            val miBundle = Bundle()
+
+            // RELLENO EL BUNDLE
+            miBundle.putString("datoActividadB", mibinding.mostrarBundleTV.text.toString())
+            // VINCULAR EL BUNDLE AL INTENT
+            miIntent.putExtras(miBundle)
+
+            setResult(Activity.RESULT_OK, miIntent) // ESTABLECER E RESULTADO
+
             finish() // FINALIZAR ACTIVIDAD
         }
     } // METODO PARA INICIALIZAR
