5d2cc915ab7dd41bfc47f75809d90a4c49a68535
estelaV9 <esteladeveg@gmail.com>
2024-12-03 09:53:25 +0100

    añadir el mibinding y el metodo para inicilizar componentes

--- Tema5_InterfaceGraficaII/Ejercicios_DentroTema/Ejemplo_IntentExplicita/app/src/main/java/com/example/ejemplo_intentexplicita/ActivityB.kt
+++ Tema5_InterfaceGraficaII/Ejercicios_DentroTema/Ejemplo_IntentExplicita/app/src/main/java/com/example/ejemplo_intentexplicita/ActivityB.kt
@@ -3,18 +3,27 @@ package com.example.ejemplo_intentexplicita
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.ejemplo_intentexplicita.databinding.ActivityBBinding
+import com.example.ejemplo_intentexplicita.databinding.ActivityMainBinding
 
 class ActivityB : AppCompatActivity() {
+    lateinit var mibinding: ActivityBBinding;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_b)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        inicializarComponentes() // LLAMAR AL METODO PARA INICIALIZAR COMPONENTES
     }
+
+    private fun inicializarComponentes(){
+        // SE INSTANCIA EL OBJETO binding
+        mibinding = ActivityBBinding.inflate(layoutInflater)
+
+        // ESTABLECE EL LAYOUT A MI ACTIVIDAD
+        setContentView(mibinding.root) // EL ROOT ES LA PROPIEDAD QUE ME PERMITE ACCEDER A LA RAIZ DEL LAYOUT
+
+        // LISTENER AL BOTON
+        mibinding.finalizarActBBtt.setOnClickListener {
+            finish()
+        }
+    } // METODO PARA INICIALIZAR
 }
