df26bd80f0aa1d47f482fdf1c09ea3c8b0e3f3f4
estelaV9 <esteladeveg@gmail.com>
2024-12-03 09:43:33 +0100

    actualizar compileSdk

--- Tema5_InterfaceGraficaII/Ejercicios_DentroTema/Ejemplo_IntentExplicita/app/build.gradle.kts
+++ Tema5_InterfaceGraficaII/Ejercicios_DentroTema/Ejemplo_IntentExplicita/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.ejemplo_intentexplicita"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.ejemplo_intentexplicita"
