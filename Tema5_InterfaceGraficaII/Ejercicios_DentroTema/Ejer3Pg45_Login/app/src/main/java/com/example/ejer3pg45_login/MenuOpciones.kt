6bccf7885a1a7d0cc33c08d9d71da7061abc2f30
estelaV9 <esteladeveg@gmail.com>
2025-01-07 17:57:40 +0100

    Añadir la logica de cuando se pulse cada uno de las opcioens del menu que vaya a su respectiva pantalla

--- Tema5_InterfaceGraficaII/Ejercicios_DentroTema/Ejer3Pg45_Login/app/src/main/java/com/example/ejer3pg45_login/MenuOpciones.kt
+++ Tema5_InterfaceGraficaII/Ejercicios_DentroTema/Ejer3Pg45_Login/app/src/main/java/com/example/ejer3pg45_login/MenuOpciones.kt
@@ -1,8 +1,12 @@
 package com.example.ejer3pg45_login
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.ejer3pg45_login.databinding.ActivityMenuOpcionesBinding
+import com.example.ejer3pg45_login.menu_options_screen.EnviarSMS
+import com.example.ejer3pg45_login.menu_options_screen.Llamar
+import com.example.ejer3pg45_login.menu_options_screen.Navegar
 
 class MenuOpciones : AppCompatActivity() {
     private lateinit var mibinding: ActivityMenuOpcionesBinding
@@ -12,5 +16,20 @@ class MenuOpciones : AppCompatActivity() {
         // INICIALIZAMOS ViewBinding
         mibinding = ActivityMenuOpcionesBinding.inflate(layoutInflater)
         setContentView(mibinding.root)
+
+        mibinding.navegarBtt.setOnClickListener {
+            val miIntent = Intent(this, Navegar::class.java)
+            startActivity(miIntent)
+        } // IR A LA PANTALLA NAVEGAR
+
+        mibinding.llamarBtt.setOnClickListener {
+            val miIntent = Intent(this, Llamar::class.java)
+            startActivity(miIntent)
+        } // IR A LA PANTALLA LLAMAR
+
+        mibinding.enviarSmsBtt.setOnClickListener {
+            val miIntent = Intent(this, EnviarSMS::class.java)
+            startActivity(miIntent)
+        } // IR A LA PANTALLA DE ENVIAR UN SMS
     }
 }
