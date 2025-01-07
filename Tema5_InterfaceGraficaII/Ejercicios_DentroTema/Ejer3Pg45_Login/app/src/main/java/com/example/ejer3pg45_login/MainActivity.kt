f75efaed9c8f0d98042256780fd1836533da0f5a
estelaV9 <esteladeveg@gmail.com>
2025-01-07 16:48:14 +0100

    Añadir un textview que sirve como alerta para si las credenciales no coinciden y si coinciden redirigir al usuario a otra pantalla

--- Tema5_InterfaceGraficaII/Ejercicios_DentroTema/Ejer3Pg45_Login/app/src/main/java/com/example/ejer3pg45_login/MainActivity.kt
+++ Tema5_InterfaceGraficaII/Ejercicios_DentroTema/Ejer3Pg45_Login/app/src/main/java/com/example/ejer3pg45_login/MainActivity.kt
@@ -1,20 +1,40 @@
 package com.example.ejer3pg45_login
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.Menu
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.ejer3pg45_login.databinding.ActivityMainBinding
+import com.example.ejer3pg45_login.model.User
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mibinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // INICIALIZAMOS ViewBinding
+        mibinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mibinding.root) // SE VINCULA A LA RAIZ DEL LAYOUT
+
+        // USUARIO QUE PODRA ACCEDER AL SISTEMA
+        var user = User("admin", "admin")
+
+        mibinding.alertTV.visibility = View.INVISIBLE // SE INCIA INVISIBLE LA ALERTA
+
+        mibinding.entrarBtt.setOnClickListener {
+            if (user.isValid(
+                    mibinding.nameUserET.text.toString(),
+                    mibinding.passwordET.text.toString()
+                )
+            ) {
+                // SI LAS CREDENCIALES COINCIDEN, REDIRIGIRA AL SISTEMA
+                val miIntent = Intent(this, MenuOpciones::class.java)
+                startActivity(miIntent)
+            } else {
+                // SI LAS CREDENCIALES NO COINCIDEN, SE MUESTRA LA ALERTA
+                mibinding.alertTV.visibility = View.VISIBLE
+            } // SE VALIDA QUE SEAN LAS CREDENCIALES DEL USUARIO
+        } // CUANDO SE PULSE EL BOTON SE VALIDARA Y ENTRARA EN EL SISTEMA
     }
 }
