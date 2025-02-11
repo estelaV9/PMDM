2d9f9199e4bd809ba94756a7975d8bf48f8ed02f
estelaV9 <esteladeveg@gmail.com>
2025-02-11 10:36:08 +0100

    implementar el toolbar con las opciones y el menu inferior

--- Repaso/RepasoDelTema/app/build.gradle.kts
+++ Repaso/RepasoDelTema/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.repasodeltema"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.repasodeltema"
@@ -15,6 +15,10 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildFeatures{
+        viewBinding = true
     }
 
     buildTypes {
--- Repaso/RepasoDelTema/app/src/main/java/com/example/repasodeltema/MainActivity.kt
+++ Repaso/RepasoDelTema/app/src/main/java/com/example/repasodeltema/MainActivity.kt
@@ -1,20 +1,75 @@
 package com.example.repasodeltema
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.repasodeltema.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var miBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // INFLAR EL BINGING
+        miBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(miBinding.root)
+
+        // SE CONFIGURA EL TOOLBAR
+        setSupportActionBar(findViewById(R.id.my_toolbar))
+
+        // SE CONFIGURA EL BOTTON NAVIGATION
+        miBinding.bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.irPrincipal -> {
+                    Toast.makeText(this, "Inicio seleccionado", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.irFavoritos -> {
+                    Toast.makeText(this, "Favoritos seleccionado", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.irAjustes -> {
+                    Toast.makeText(this, "Ajustes seleccionado", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // SE EJECUTA AUTOMATICAMENTE CUANDO CREE LA ACTIVITY
+        menuInflater.inflate(R.menu.menu_opciones, menu)
+        return true
+    } // SE INFLA EL MENU DE OPCIONES
+
+    // SE MANEJA LA SELECCION DE OPCIONES DEL MENU
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_option1 -> {
+                Toast.makeText(this, "Opción 1 seleccionada", Toast.LENGTH_SHORT).show()
+                true
+            }
+
+            R.id.action_option2 -> {
+                Toast.makeText(this, "Opción 2 seleccionada", Toast.LENGTH_SHORT).show()
+                true
+            }
+
+            R.id.action_about -> {
+                Toast.makeText(this, "Acerca de...", Toast.LENGTH_SHORT).show()
+                true
+            }
+
+            R.id.action_exit -> {
+                finish() // CERRAR ACTIVIDAD
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
