cb70680863cf4403be58d304b9215e715fe71dae
estelaV9 <esteladeveg@gmail.com>
2025-02-11 23:06:34 +0100

    crear el fragmento y el layout de panic donde se cargara un gif

---
+++ Repaso/RepasoRecyclerView/app/src/main/java/com/example/repasorecyclerview/fragment/panic_fragment.kt
@@ -0,0 +1,65 @@
+package com.example.repasorecyclerview.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.example.repasorecyclerview.R
+
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+class PanicFragment : Fragment() {
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // INFLAMOS LA VISTA DEL FRAGMENTO
+        val rootView = inflater.inflate(R.layout.fragment_panic, container, false)
+
+        // ACCEDEMOS AL ImageView DENTRO DE LA VISTA INFLADA
+        val imageView: ImageView = rootView.findViewById(R.id.imgPanic)
+
+        // CARGAMOS EL GIF
+        Glide.with(requireContext())  // SE USA EL CONTEXTO DE LA ACTIVIDAD
+            .asGif()  // ESPECIFICAMOS QUE ES UN GIF
+            .load("https://i.pinimg.com/originals/17/62/0a/17620a76ae319084b457177d73dcc5ab.gif")  // URL DEL GIF
+            .into(imageView)  // DONDE SE DEBE MOSTRAR EL GIF
+
+        return rootView
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment panic_fragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            PanicFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
