8cba11e33f5e014878458a948dccce49095b8d15
estelaV9 <esteladeveg@gmail.com>
2025-02-11 23:06:05 +0100

    añadir el bottonnavigation y el fragmento

--- Repaso/RepasoRecyclerView/app/src/main/java/com/example/repasorecyclerview/MainActivity.kt
+++ Repaso/RepasoRecyclerView/app/src/main/java/com/example/repasorecyclerview/MainActivity.kt
@@ -3,18 +3,47 @@ package com.example.repasorecyclerview
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.repasorecyclerview.databinding.ActivityMainBinding
+import com.example.repasorecyclerview.fragment.PanicFragment
+import com.example.repasorecyclerview.fragment.RecyclerFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // EL RECYCLERVIEW FUNCIONA CON LISTAS Y CREARA UN ITEM/OBEJTO POR CADA VALOR EN LA LISTA
+        //var myList = listOf("Example1", "Example2", "Example3") // CREARIA 3 CELDAS
+
+        // CUANDO SE CREE, LLAMA AL METODO
+        //initRecyclerView()
+        initFragment()
+
+    }
+
+    private fun initFragment(){
+        // SE CARGA EL FRAGMENTO POR DEFECTO
+        loadFragment(RecyclerFragment())
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNav)
+        bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.recycler -> loadFragment(RecyclerFragment())
+                R.id.panic -> loadFragment(PanicFragment())
+            }
+            true
         }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainerBottomNav, fragment)
+            .commit()
     }
 }
