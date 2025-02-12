f3ac9eff11cd176c23e55c390926a367701e4edb
estelaV9 <esteladeveg@gmail.com>
2025-02-12 09:47:33 +0100

    añadir el apartado de enviar la url de la imagen por correo

--- Repaso/RepasoRecyclerView/app/src/main/java/com/example/repasorecyclerview/fragment/panic_fragment.kt
+++ Repaso/RepasoRecyclerView/app/src/main/java/com/example/repasorecyclerview/fragment/panic_fragment.kt
@@ -1,11 +1,14 @@
 package com.example.repasorecyclerview.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.example.repasorecyclerview.R
 
@@ -33,6 +36,7 @@ class PanicFragment : Fragment() {
 
         // ACCEDEMOS AL ImageView DENTRO DE LA VISTA INFLADA
         val imageView: ImageView = rootView.findViewById(R.id.imgPanic)
+        val buttonEnviar: Button = rootView.findViewById(R.id.enviarCorreoBtt)
 
         // CARGAMOS EL GIF
         Glide.with(requireContext())  // SE USA EL CONTEXTO DE LA ACTIVIDAD
@@ -40,8 +44,26 @@ class PanicFragment : Fragment() {
             .load("https://i.pinimg.com/originals/17/62/0a/17620a76ae319084b457177d73dcc5ab.gif")  // URL DEL GIF
             .into(imageView)  // DONDE SE DEBE MOSTRAR EL GIF
 
+        buttonEnviar.setOnClickListener {
+            // ENVIAMOS LA URL
+            enviarMensajeUrl("https://i.pinimg.com/originals/17/62/0a/17620a76ae319084b457177d73dcc5ab.gif")
+        } // CUANDO SE PULSA EL BOTON DE ENVIAR, SE MANDA LA URL
         return rootView
     }
+
+    fun enviarMensajeUrl(mensaje: String) {
+        val intent = Intent(Intent.ACTION_SEND).apply {
+            type = "text/plain"
+            putExtra(Intent.EXTRA_TEXT, mensaje)
+        }
+        // COMO ES UN FRAGMENT, EN VEZ DE packageManager SE PONE requireActivity().packageManager
+        if (intent.resolveActivity(requireActivity().packageManager) != null) {
+            startActivity(Intent.createChooser(intent, "Compartir mensaje con"))
+        } else {
+            // COMO ES UN FRAGMENT, EN VEZ DE this SE PONE requireContext()
+            Toast.makeText(requireContext(), "No hay aplicaciones disponibles para enviar el mensaje.", Toast.LENGTH_SHORT).show()
+        }
+    } // METODO PARA ENVIAR LA URL
 
     companion object {
         /**
