d30986255a96ba8764b1b6a975c56905aa9d1ca0
estelaV9 <esteladeveg@gmail.com>
2025-02-12 11:09:14 +0100

    cuando pulse el boton de añadir le llevara a la pantalla del formulario

--- Repaso/RepasoRecyclerView/app/src/main/java/com/example/repasorecyclerview/fragment/recycler_fragment.kt
+++ Repaso/RepasoRecyclerView/app/src/main/java/com/example/repasorecyclerview/fragment/recycler_fragment.kt
@@ -1,5 +1,6 @@
 package com.example.repasorecyclerview.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,19 +9,25 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.repasorecyclerview.Adapter.SuperHeroAdapter
+import com.example.repasorecyclerview.FormularioCharacter
 import com.example.repasorecyclerview.R
 import com.example.repasorecyclerview.SuperHeroProvider
+import com.example.repasorecyclerview.databinding.ActivityRecyclerFragmentBinding
 
 class RecyclerFragment : Fragment() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: SuperHeroAdapter
+    private lateinit var binding: ActivityRecyclerFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = ActivityRecyclerFragmentBinding.inflate(layoutInflater)
         // INFLAMOS EL LAYOUT DEL FRAMENTO
-        return inflater.inflate(R.layout.activity_recycler_fragment, container, false)
+        // si no tenemos el binding:
+        // return inflater.inflate(R.layout.activity_recycler_fragment, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -31,6 +38,13 @@ class RecyclerFragment : Fragment() {
 
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
         recyclerView.adapter = adapter
+
+        binding.addCharacter.setOnClickListener {
+            var intent = Intent(requireContext(), FormularioCharacter::class.java)
+            //intent.putExtra("heroe",SuperHeroProvider.superHeroList.get(pos)) pos si se necesita pasar algo al intent.
+            startActivity(intent)
+        } // CUANDO PULSES EL BOTON DE AÑADIR TE LLEVAA A UNA NUEVA ACTIVITY CON LOS DATOS A RELLENAR
+
 
         /*private fun initRecyclerView(){
             val recyclerView = findViewById<RecyclerView>(R.id.recyclerSuperHero)
